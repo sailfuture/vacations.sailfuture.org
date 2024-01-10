d66036c9a103304d2a403170d9b29f73ae6c75d2
sailfuture <78326976+sailfuture@users.noreply.github.com>
2024-01-09 19:15:16 -0500

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,8 @@ module.exports = {
     },
   },
   variants: {},
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('flowbite/plugin'),
+  ],
 };
